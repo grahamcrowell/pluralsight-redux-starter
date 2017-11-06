2c6b71bea3fbe3679a96820855feeb0d6c9664cf
gcrowell <graham.crowell@visiercorp.com>
2017-11-05 19:55:51 -0800

    courses boiler plate

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -6,6 +6,8 @@ const Header = () => {
             <IndexLink to ="/" activeClassName="active">Home</IndexLink>
             {" | "}
             <Link to="/courses" activeClassName="active">Courses</Link>
+            {" | "}
+            <Link to="/about" activeClassName="active">About</Link>
         </nav>
     );
 };
---
+++ src/components/course/CoursesPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class CoursesPage extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Courses</h1>
+        <p>This app uses React</p>
+      </div>
+    );
+  }
+}
+
+export default CoursesPage;
