604b32bb7792778bd2ae35c42458fe4de5d78aa7
gcrowell <graham.crowell@visiercorp.com>
2017-11-05 20:09:07 -0800

    Add course; alert onClickSave

--- src/components/course/CoursesPage.js
+++ src/components/course/CoursesPage.js
@@ -1,11 +1,37 @@
 import React from 'react';
 
 class CoursesPage extends React.Component {
+  // create new course form; component container
+  constructor(props, context) {
+    super(props, context);
+
+    // set local state
+    this.state = {
+      course: {title: null}
+    };
+  }
+  // called on each key stroke, updates state.
+  onTitleChange(event){
+    const course = this.state.course;
+    course.title = event.target.value;
+    this.setState({course: course});
+  }
+  onClickSave() {
+    alert(`Saving course: ${this.state.course.title}`)
+  }
   render() {
     return (
       <div>
         <h1>Courses</h1>
-        <p>This app uses React</p>
+        <h2>Add Course</h2>
+        <input
+        type="text"
+        onChange={this.onTitleChange} // calls function above
+        value={this.state.course.title} />
+        <input
+        type="submit"
+        onClick={this.onClickSave} // calls function above
+        value="Save" />
       </div>
     );
   }
